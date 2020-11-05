18a4fc039ef67f0c1dc52deeb40177e42997c931
Santiago Medina <santiago.medina@pragma.com.co>
2020-11-05 10:05:42 -0500

    saved commit

--- app/src/main/java/com/sanmed/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/sanmed/android/aboutme/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.sanmed.android.aboutme
 
+import android.content.Context
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.sanmed.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName:MyName = MyName("Santiago Medina Chaverra")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            myName?.nickName = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+        }
+
+
+        //Hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
---
+++ app/src/main/java/com/sanmed/android/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.sanmed.android.aboutme
+
+data class MyName (var name:String = "", var nickName:String = "")
